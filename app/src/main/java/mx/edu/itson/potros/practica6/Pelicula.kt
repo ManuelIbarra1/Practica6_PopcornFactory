18c71be9925066ee7a7e1a33cad8c08e5d2658c4
ManuelIbarra1 <manuel.ibarra@potros.itson.edu.mx>
2025-03-12 17:55:52 -0700

    commit

--- app/src/main/java/mx/edu/itson/potros/practica6/Pelicula.kt
+++ app/src/main/java/mx/edu/itson/potros/practica6/Pelicula.kt
@@ -1,6 +1,5 @@
 package mx.edu.itson.potros.practica6
 
-data class Pelicula(var titulo: String,
-                    var image: Int,
-                    val header: Int,
-                    var sinopsis: String)
+data class Pelicula(var titulo: String, var image: Int, val header: Int, var sinopsis: String, var seats: ArrayList<Cliente>) {
+
+}
--- app/src/main/java/mx/edu/itson/potros/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/itson/potros/practica6/detalle_pelicula.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.potros.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -16,12 +18,33 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val seatLeft: TextView = findViewById(R.id.seatLeft)
+        val buyTickets: Button = findViewById(R.id.buyTickets)
 
+        var ns = 0
+        var id = -1
+        var title = ""
         val bundle = intent.extras
         if (bundle != null) {
+            ns = bundle.getInt("numberSeats")
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seatLeft.setText("$ns seats available")
+        }
+
+        if (ns == 0) {
+            buyTickets.isActivated = false
+        } else {
+            buyTickets.isActivated = true
+            buyTickets.setOnClickListener {
+                var intent: Intent = Intent(this, SeatSelection::class.java)
+
+                intent.putExtra("id", id)
+                intent.putExtra("name", title)
+
+                this.startActivity(intent)
+            }
         }
     }
 }
